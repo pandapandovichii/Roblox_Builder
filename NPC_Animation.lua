d0fba12c569a4f3367938718fec63a62197bb150
TTA-Dev-Alex <114209303+TTA-Dev-Alex@users.noreply.github.com>
2022-09-23 12:19:02 +0700

    Update and rename NPC_Animation to NPC_Animation.lua

--- NPC_Animation.lua
+++ NPC_Animation.lua
@@ -1,6 +1,6 @@
-–make the anim, copy the ID
-–insert script into npc, insert anim into script
-–add the anim id into the animation properties
+--make the anim, copy the ID
+--insert script into npc, insert anim into script
+--add the anim id into the animation properties
 local animation = script:WaitForChild('Animation')
 local humanoid = script.Parent:WaitForChild('Humanoid')
 local dance = humanoid:LoadAnimation(animation)
