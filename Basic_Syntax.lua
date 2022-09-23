c7bd22d38bafb8d1a840c14e9cfa3e6cdd454b41
TTA-Dev-Alex <114209303+TTA-Dev-Alex@users.noreply.github.com>
2022-09-23 12:43:45 +0700

    Create Basic_Syntax.lua

---
+++ Basic_Syntax.lua
@@ -0,0 +1,20 @@
+-- You make a comment by using two minus signs
+-- the computer cannot read lines that are a comment
+
+-- you print a message like this:
+print("the player can see this message")
+
+-- you can create a variable like this
+number = 1
+
+--then you can check the value of a variable by using double equals signs
+if number == 1 then
+  print("yep, thats a one)
+end
+  
+-- we can use loops to make things run forever, like this
+while true do
+    print("the loop is running")
+    wait(5) -- this is important, if you run a forever loop without waiting, your computer will crash
+end
+  
