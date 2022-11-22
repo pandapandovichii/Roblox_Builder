819b16685513665f5355d75dce1dbd8e69e62b20
TTA-Dev-Alex <114209303+TTA-Dev-Alex@users.noreply.github.com>
2022-11-22 12:30:48 +0700

    Create check-has-child.lua

---
+++ check-has-child.lua
@@ -0,0 +1,5 @@
+-- check for child in a location
+hasChild = part:FindFirstChild("otherPart") -- put the name of the child here
+
+--if the child is found, hasChild will == True
+--if the child is not found, hasChild will == False
