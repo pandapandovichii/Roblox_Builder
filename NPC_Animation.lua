8dea99205ca1a0b86effe74e8d7d2e113bc0d31b
pandapandovichii <glebbogachev00@gmail.com>
2024-06-28 14:04:36 +0700

    Update NPC_Animation.lua
    Added another version of the script

--- NPC_Animation.lua
+++ NPC_Animation.lua
@@ -1,3 +1,4 @@
+-- Working Version 1
 --make the anim, copy the ID
 --insert script into npc, insert anim into script
 --add the anim id into the animation properties
@@ -6,3 +7,11 @@ local humanoid = script.Parent:WaitForChild('Humanoid')
 local action = humanoid:LoadAnimation(animation)
 action:Play()
 
+-- Working Version 2
+-- Put this inside the NPC
+-- Paste the animation id  "rbxassetid://[animationid]"
+local animation = Instance.new("Animation")
+animation.AnimationId = "rbxassetid://156712"
+
+local animationTrack = script.Parent.Humanoid.Animator:LoadAnimation(animation)
+animationTrack:Play()
